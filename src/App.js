ce2266e8d3fe19e7c647cabc282992e1c2ba5a44
tilda21 <tilda21@git.com>
2019-11-18 18:08:51 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        title: '',
+        globalTitle: '',
+      };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({title: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.setState({globalTitle:  'My form - '});
+    console.log('Title changed');
+    event.preventDefault();
+  }
+
+  render() {
+    console.log('Rendered Form');
+    return (
+      <>
+        <h1>{this.state.globalTitle}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Text:
+            <input 
+                type="text" 
+                value={this.state.title} 
+                onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </>
+    );
+  }
 }
+
 
 export default App;
